df421da892c4b3d3734640e9a972ad5ee6d6cf56
André Boddenberg <andre.boddenberg@cortado.com>
2014-07-29 13:44:14 +0200

    anbod: init commit!

---
+++ ListJSONTest2/src/com/cortado/listjsontest/test/FI_test.java
@@ -0,0 +1,16 @@
+package com.cortado.listjsontest.test;
+
+import android.test.AndroidTestCase;
+
+public class FI_test extends AndroidTestCase{
+	
+	public void testBla()
+	{
+		assertEquals(0,0);
+	}
+	
+	public void testBla2()
+	{
+		assertEquals(20,20);
+	}
+}
