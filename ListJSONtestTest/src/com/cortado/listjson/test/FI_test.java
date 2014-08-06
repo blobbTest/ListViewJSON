4cdd6ec8b3563684a669d99d33fba480b649d24a
André Boddenberg <andre.boddenberg@cortado.com>
2014-08-06 12:30:11 +0200

    fwfw

---
+++ ListJSONtestTest/src/com/cortado/listjson/test/FI_test.java
@@ -0,0 +1,16 @@
+package com.cortado.listjson.test;
+
+import android.test.AndroidTestCase;
+
+public class FI_test extends AndroidTestCase{
+	
+	public void testBla()
+	{
+		assertEquals(0,0);
+	}
+	
+	public void testBla2()
+	{
+		assertEquals(20,20);
+	}
+}
